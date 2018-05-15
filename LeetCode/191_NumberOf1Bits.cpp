1909167a2ad2fd7cf31acf1397e689f1b43e8879
Rohit Sharma <rohshar@microsoft.com>
2018-05-15 23:39:29 +0530

    bug in hammingWeight

--- LeetCode/191_NumberOf1Bits.cpp
+++ LeetCode/191_NumberOf1Bits.cpp
@@ -6,8 +6,9 @@ using namespace std;
 int hammingWeight(uint32_t n)
 {
     int i = 32, result = 0;
-    while (i--) {
-        result += (n ^ 1);
+    while (i-- && n > 0) {
+        result += (n & 1);
         n >>= 1;
     }
+    return result;
 }
