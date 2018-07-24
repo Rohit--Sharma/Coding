b9e39370bf57f2d6c0f7b4fb59b55ae98c10c832
Rohit Sharma <rohshar@microsoft.com>
2018-07-24 20:48:04 +0530

    LIS start

---
+++ LeetCode/300_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+    }
+};
+
+int main()
+{
+    vector<int> input {10,9,2,5,3,7,101,18};
+    cout << Solution().lengthOfLIS(input) << endl;
+    
+    return 0;
+}
