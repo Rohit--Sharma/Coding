3df3a823ae15081b95163d1d8c2a71ce9dbb5986
Rohit <rohit.sharma@wisc.edu>
2018-10-28 16:09:11 -0500

    Questions asked in first Google coding round

--- Others/000_dth_ancestor_array.cpp
+++ Others/000_dth_ancestor_array.cpp
@@ -42,6 +42,79 @@ vector<int> dth_ancestor(vector<int> parent, int d)
     return result;
 }
 
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+/*
+ * Bruteforce
+ */
+/*
+int dth_ancestor(int node, int D, vector<int> A) {
+    for (int i = 0; i < D; i++) {
+        if (A[node] == -1)
+            return -1;
+        node = A[node];
+    }
+    return node;
+}
+
+vector<int> solution(int D, vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    vector<int> result;
+    for (unsigned int i = 0; i < A.size(); i++) {
+        result.push_back(dth_ancestor(i, D, A));
+    }    
+    return result;
+}
+*/
+
+// DP approach
+
+int store_nth_ancestor_and_return(vector<int> A, vector<vector<int>> &dp, int node, int n) {
+    if (dp[node][n] != -2) {
+        return dp[node][n];
+    }
+    
+    if (A[node] == -1) {
+        dp[node][n] = -1;
+        return -1;
+    }
+    
+    int res = store_nth_ancestor_and_return(A, dp, A[node], n - 1);
+    dp[A[node]][n - 1] = res;
+    return res;
+}
+
+vector<int> solution(int D, vector<int> &A) {
+    vector<int> result;
+    
+    vector<vector<int>> dp(A.size(), vector<int>(D + 1, -2));
+    for (unsigned int i = 0; i < A.size(); i++) {
+        dp[i][1] = A[i];
+        if (A[i] == -1) {
+            for (unsigned int j = 2; (int)j < D + 1; j++) {
+                dp[i][j] = -1;
+            }    
+        }
+    }
+    
+    for (unsigned int i = 0; i < A.size(); i++) {
+        //cout << dp[i][D] << endl;
+        if (dp[i][D] != -2) {
+            result.push_back(dp[i][D]);
+        }
+        else {
+            result.push_back(store_nth_ancestor_and_return(A, dp, i, D));
+        }
+    }
+    
+    return result;
+}
+
 int main()
 {
     vector<int> input {1, 3, 4, 5, 7, 3, 7, 5, 2, 1};
--- Others/001_closest_store_location.cpp
+++ Others/001_closest_store_location.cpp
@@ -85,6 +85,80 @@ vector<int> closest_stores(vector<int> houses, vector<int> stores)
     return nearest_stores;
 }
 
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+vector<int> solution(vector<int> &stores, vector<int> &houses) {
+    // write your code in C++14 (g++ 6.2.0)
+    vector<int> closest_houses;
+    
+    sort(stores.begin(), stores.end());
+    //cout << "Stores: ";
+    //for (int store : stores)
+    //    cout << store << " ";
+    //cout << endl;
+    for (int house : houses) {
+        int lo = 0, hi = stores.size() - 1;
+        int mid;
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (house <= stores[lo]) {
+                closest_houses.push_back(stores[lo]);
+                break;
+            }
+            if (house >= stores[hi]) {
+                closest_houses.push_back(stores[hi]);
+                break;
+            }
+            if (house == stores[mid]) {
+                closest_houses.push_back(stores[mid]);
+                break;
+            }
+            else if (house < stores[mid]) {
+                if (house > stores[mid - 1]) {
+                    int left_store_dist = house - stores[mid - 1];
+                    int right_store_dist = stores[mid] - house;
+                    int min_dist = min(left_store_dist, right_store_dist);
+                    if (left_store_dist == min_dist) {
+                        closest_houses.push_back(stores[mid - 1]);
+                        break;
+                    }
+                    else {
+                        closest_houses.push_back(stores[mid]);
+                        break;
+                    }
+                }
+                else {
+                    hi = mid - 1;
+                    continue;
+                }
+            }
+            else if (house > stores[mid]) {
+                if (house < stores[mid + 1]) {
+                    int left_store_dist = house - stores[mid];
+                    int right_store_dist = stores[mid + 1] - house;
+                    int min_dist = min(left_store_dist, right_store_dist);
+                    if (left_store_dist == min_dist) {
+                        closest_houses.push_back(stores[mid]);
+                        break;
+                    }
+                    else {
+                        closest_houses.push_back(stores[mid + 1]);
+                        break;
+                    }
+                }  
+                else {
+                    lo = mid + 1;
+                    continue;
+                }
+            }
+        }
+    }
+    
+    return closest_houses;
+}
+
 int main()
 {
     vector<int> houses_loc {11, 5, 2, 14, 1, 3, 7, 16, 6, 13, 8};
