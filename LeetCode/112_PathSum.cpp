e60fbeeed60a457741808b91dc3fd351309c0b03
Rohit <rohit.sharma@wisc.edu>
2019-05-26 19:05:24 -0400

    Check if there is a path with a sum in a binary tree

--- LeetCode/112_PathSum.cpp
+++ LeetCode/112_PathSum.cpp
@@ -27,6 +27,22 @@ struct TreeNode {
 	TreeNode (int x) : val(x), left(NULL), right(NULL) {}
 };
 
+class Solution {
+public:
+	bool hasPathSum(TreeNode* root, int sum) {
+		if(!root)
+			return false;
+		if(!root->left && !root->right) {
+			if(root->val == sum)
+				return true;
+			else
+				return false;
+		}
+		
+		return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+	}
+};
+
 int main() {
 
 	return 0;
