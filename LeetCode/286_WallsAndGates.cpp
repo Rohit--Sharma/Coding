c07f72c1e73ffa710d71e670872d67dd0fb6ab49
Rohit <rohit.sharma@wisc.edu>
2019-05-27 19:49:40 -0400

    A breadth first search problem with a nice idea

--- LeetCode/286_WallsAndGates.cpp
+++ LeetCode/286_WallsAndGates.cpp
@@ -28,7 +28,8 @@ struct TreeNode {
 	TreeNode (int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution {
+// First find all the gates, then expand the whole rooms array outwards from each gate.
+class SolutionBruteForce {
 public:
 	void wallsAndGates(vector<vector<int>>& rooms) {
 		vector<vector<int>> gate_locations = getGateLocs(rooms);
@@ -63,14 +64,12 @@ private:
 		int dist = -1;
 		search_space.push(root);
 		visited.insert(root);
-		// cout << "Root (" << root[0] << ", " << root[1] << ")\n";
 		while(!search_space.empty()) {
 			dist++;
 			int size = search_space.size();
 			for(int i = 0; i < size; i++) {
 				vector<int> curr = search_space.front();
 				if(rooms[curr[0]][curr[1]] != -1 || rooms[curr[0]][curr[1]] != 0) {
-					// cout << "Exploring (" << curr[0] << ", " << curr[1] << "), Dist: " << dist << "\n";
 					rooms[curr[0]][curr[1]] = min(rooms[curr[0]][curr[1]], dist);
 
 					// add successors to queue
@@ -78,12 +77,7 @@ private:
 						if(curr[0] + succ_r[j] >= 0 && curr[0] + succ_r[j] < rooms.size() && curr[1] + succ_c[j] >= 0 && curr[1] + succ_c[j] < rooms[0].size()) {
 							if(rooms[curr[0] + succ_r[j]][curr[1] + succ_c[j]] != -1 && rooms[curr[0] + succ_r[j]][curr[1] + succ_c[j]] != 0) {
 								vector<int> succ {curr[0] + succ_r[j], curr[1] + succ_c[j]};
-								// cout << "\tSuccessor (" << succ[0] << ", " << succ[1] << ")\nVisited Nodes:\n";
-								// for (auto it = visited.begin(); it != visited.end(); it++) {
-								// 	cout << "\t(" << (*it)[0] << ", " << (*it)[1] << ")\n";
-								// }
 								if (visited.find(succ) == visited.end()) {
-									// cout << "\tAdding successor\n";
 									search_space.push(succ);
 									visited.insert(succ);
 								}
@@ -95,6 +89,46 @@ private:
 			}
 		}
 	}
+};
+
+// More compact implementation of the above approach: Expand outwards from each gate at once
+class Solution {
+public:
+	void wallsAndGates(vector<vector<int>>& rooms) {
+		int m = rooms.size();
+		if(m == 0) return;
+		int n = rooms[0].size();
+		queue<vector<int>> search_space;
+
+		// Push all the gates locations in the search space (open list) of the queue so that each iteration of bfs expands all the queues at once. 
+		//   Therefore, one pass should be able to fill in the rooms array.
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				if(rooms[i][j] == 0) {
+					search_space.push(vector<int> {i, j});
+				}
+			}
+		}
+
+		while(!search_space.empty()) {
+			vector<int> curr = search_space.front();
+			search_space.pop();
+			for(int dir = 0; dir < 4; dir++) {
+				int r = curr[0] + succ_r[dir];
+				int c = curr[1] + succ_c[dir];
+				// We only need one pass, so we can directly compare with INF. Also because of this, we don't need to maintain which nodes are visited.
+				if(r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != INF)
+					continue;
+				rooms[r][c] = rooms[curr[0]][curr[1]] + 1;
+				search_space.push(vector<int> {r, c});
+			}
+		}
+	}
+
+private:
+	int INF = 2147483647;
+	vector<int> succ_r {-1, 0, 1, 0};
+	vector<int> succ_c {0, 1, 0, -1};
 };
 
 int main() {
