0888cc49c2c601a15288c85fadf7173cced45101
Rohit <rohit.sharma@wisc.edu>
2019-09-14 21:29:13 -0500

    Update

--- LeetCode/253_MeetingRooms2.cpp
+++ LeetCode/253_MeetingRooms2.cpp
@@ -16,10 +16,10 @@ using namespace std;
  * Definition for an interval.
  */
 struct Interval {
-    int start;
-    int end;
-    Interval() : start(0), end(0) {}
-    Interval(int s, int e) : start(s), end(e) {}
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
 };
 
 /**
@@ -28,41 +28,40 @@ struct Interval {
  */
 class Solution {
 public:
-    int minMeetingRooms(vector<Interval>& intervals) {
+	int minMeetingRooms(vector<Interval>& intervals) {
 		if (intervals.size() == 0)
 			return 0;
 
-        vector<Time> times;
-        for (auto interval : intervals) {
-            times.push_back(Time(interval.start, true));
-            times.push_back(Time(interval.end, false));
-        }
+		vector<Time> times;
+		for (auto interval : intervals) {
+			times.push_back(Time(interval.start, true));
+			times.push_back(Time(interval.end, false));
+		}
 
-        sort(times.begin(), times.end(), less_than());
+		sort(times.begin(), times.end(), less_than());
 		int num_curr_meet_rooms = 0, min_meet_rooms = 0;
 		for (int i = 0; i < times.size(); i++)
 			if (times[i].is_start) {
 				num_curr_meet_rooms++;
-                if (num_curr_meet_rooms > min_meet_rooms)
-                    min_meet_rooms = num_curr_meet_rooms;
-            } else {
-                num_curr_meet_rooms--;
-            }
+				min_meet_rooms = max(min_meet_rooms, num_curr_meet_rooms);
+			} else {
+				num_curr_meet_rooms--;
+			}
 
 		return min_meet_rooms;
-    }
+	}
 
 private:
-    struct Time {
-        int time;
-        bool is_start;
-        Time(int t, bool st) : time(t), is_start(st) {}
-    };
+	struct Time {
+		int time;
+		bool is_start;
+		Time(int t, bool st) : time(t), is_start(st) {}
+	};
 
 	struct less_than {
 		inline bool operator() (const Time& t1, const Time& t2) {
-            if (t1.time == t2.time)
-                return t1.is_start == false;
+			if (t1.time == t2.time)
+				return t1.is_start == false;
 			return t1.time < t2.time;
 		}
 	};
@@ -71,54 +70,50 @@ private:
 // Another approach
 class Solution_II {
 public:
-    vector<vector<Interval>> rooms;
-    
-    int minMeetingRooms(vector<Interval>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](const Interval a, const Interval b){
-            return a.start < b.start;
-        });
-        
-        for (Interval interval : intervals)
-        {
-            AddMeeting(interval);
-        }
-        return rooms.size();
-    }
-    
-    void AddMeeting(Interval interval)
-    {
-        
-        for (int i = 0; i < rooms.size(); i++)
-        {
-            if (isValid(interval, rooms[i])) return;
-        }
-        
-        rooms.push_back({interval});
-    }
-    
-    bool isValid(Interval interval, vector<Interval> &room)
-    {
-        Interval roomInterval = room.back();
-        if (interval.start < roomInterval.end) return false;
-        room.push_back(interval);
-        return true;
-    }
+	int minMeetingRooms(vector<Interval>& intervals) {
+		sort(intervals.begin(), intervals.end(), [](const Interval a, const Interval b){
+			return a.start < b.start;
+		});
+		
+		for (Interval interval: intervals) {
+			AddMeeting(interval);
+		}
+		return rooms.size();
+	}
+
+private:
+	vector<vector<Interval>> rooms;
+
+	void AddMeeting(Interval interval) {
+		for (int i = 0; i < rooms.size(); i++) {
+			if (isValid(interval, rooms[i])) return;
+		}
+
+		rooms.push_back({interval});
+	}
+	
+	bool isValid(Interval interval, vector<Interval> &room) {
+		Interval roomInterval = room.back();
+		if (interval.start < roomInterval.end) return false;
+		room.push_back(interval);
+		return true;
+	}
 };
 
 int main() {
-    vector<Interval> intervals;
+	vector<Interval> intervals;
 
-    intervals = {Interval(2, 11), Interval(6, 16), Interval(11, 16)};
-    cout << Solution().minMeetingRooms(intervals) << endl;
+	intervals = {Interval(2, 11), Interval(6, 16), Interval(11, 16)};
+	cout << Solution().minMeetingRooms(intervals) << endl;
 
-    intervals = {Interval(1, 4), Interval(5, 6), Interval(8, 9), Interval(2, 6)};
-    cout << Solution().minMeetingRooms(intervals) << endl;
+	intervals = {Interval(1, 4), Interval(5, 6), Interval(8, 9), Interval(2, 6)};
+	cout << Solution().minMeetingRooms(intervals) << endl;
 
-    intervals = {Interval(0, 30), Interval(5, 10), Interval(15, 20)};
-    cout << Solution().minMeetingRooms(intervals) << endl;
+	intervals = {Interval(0, 30), Interval(5, 10), Interval(15, 20)};
+	cout << Solution().minMeetingRooms(intervals) << endl;
 
-    intervals = {Interval(7, 10), Interval(2, 4)};
-    cout << Solution().minMeetingRooms(intervals) << endl;
+	intervals = {Interval(7, 10), Interval(2, 4)};
+	cout << Solution().minMeetingRooms(intervals) << endl;
 
-    return 0;
+	return 0;
 }
