dc15cfa9040db3235d808982bad71d92e023ebac
Rohit <rohit.sharma@wisc.edu>
2019-09-18 21:41:26 -0500

    Added an awesome implementation to solve subarray sum in O(n)

--- LeetCode/560_SubarraySumEqualsK.cpp
+++ LeetCode/560_SubarraySumEqualsK.cpp
@@ -48,6 +48,35 @@ public:
     }
 };
 
+class Solution_Optimal {
+public:
+    // Instead of iterating everytime on j, we can store the 
+    //   cumulative sums. For each index, if the cumulative sum at that 
+    //   index is sum[i], and if we have already cumulative sum seen sum[i] - k before, 
+    //   that means between the index when we saw sum[i] - k and i, the subarray sum is k.
+    //   We can store how many times we saw each cumulative sum in a map
+    // Time complexity: O(n), Space complexity: O(n)
+    int subarraySum(vector<int>& nums, int k) {
+        int res = 0;
+
+        unordered_map<int, int> cum_sum_counts {{0, 1}};
+        int cum_sum = 0;
+
+        for(int num: nums) {
+            cum_sum += num;
+
+            if(cum_sum_counts.find(cum_sum - k) != cum_sum_counts.end())
+                res += cum_sum_counts[cum_sum - k];
+
+            if(cum_sum_counts.find(cum_sum) == cum_sum_counts.end())
+                cum_sum_counts[cum_sum] = 0;
+            cum_sum_counts[cum_sum]++;
+        }
+
+        return res;
+    }
+};
+
 int main() {
     vector<int> inp {1, 1, 1};
     cout << Solution().subarraySum(inp, 2) << endl;
