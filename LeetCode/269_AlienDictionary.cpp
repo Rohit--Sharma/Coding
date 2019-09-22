dc71582638fe67743c87b295173bc53e6f2b1b14
Rohit <rohit.sharma@wisc.edu>
2019-09-22 10:38:16 -0700

    Amazing application of topological sort. Accepted.

--- LeetCode/269_AlienDictionary.cpp
+++ LeetCode/269_AlienDictionary.cpp
@@ -39,9 +39,15 @@ public:
         unordered_map<char, vector<char>> adj_list;
         unordered_map<char, bool> visited;
 
+        // Keeping track of recursion stack of nodes to detect a cycle if 
+        //   there is a back edge to one of the nodes already in rec stack.
+        unordered_map<char, bool> rec_stack;
+
         for(string word: words)
-            for(char ch: word)
+            for(char ch: word) {
                 visited[ch] = false;
+                rec_stack[ch] = false;
+            }
 
         for(int i = 1; i < words.size(); i++) {
             for(int idx = 0; idx < min(words[i - 1].size(), words[i].size()); idx++) {
@@ -49,15 +55,28 @@ public:
                     if(adj_list.find(words[i - 1][idx]) == adj_list.end())
                         adj_list[words[i - 1][idx]] = vector<char>();
                     adj_list[words[i - 1][idx]].push_back(words[i][idx]);
+
+                    // No need to compare the rest of the characters, only first mismatch is important
+                    break;
                 }
             }
         }
+
+        // TODO: Can this step be optimized together with topoligical sort?
+        // * Handle the corner case when the input is invalid due to a cycle.
+        if(is_cyclic(adj_list, visited, rec_stack)) {
+            return "";
+        }
+
+        // Reset visited to false
+        for(string word: words)
+            for(char ch: word)
+                visited[ch] = false;
 
         for(auto it = visited.begin(); it != visited.end(); it++)
             if(!it->second)
                 topological_sort(adj_list, it->first, visited);
 
-        // TODO: Handle the corner case when the input is invalid due to a cycle.
         string res;
         while(!order.empty()) {
             res += order.top();
@@ -69,13 +88,40 @@ public:
 private:
     stack<char> order;
 
-    // Inspired from https://www.geeksforgeeks.org/topological-sorting/
+    // * Inspired from https://www.geeksforgeeks.org/topological-sorting/
     void topological_sort(unordered_map<char, vector<char>> adj_list, char root, unordered_map<char, bool> &visited) {
         visited[root] = true;
         for(char successor: adj_list[root])
             if(!visited[successor])
                 topological_sort(adj_list, successor, visited);
         order.push(root);
+    }
+
+    bool is_cyclic_helper(unordered_map<char, vector<char>> adj_list, char node, unordered_map<char, bool> &visited, unordered_map<char, bool> &rec_stack) {
+        if(!visited[node]) {
+            visited[node] = true;
+            rec_stack[node] = true;
+
+            for(char successor: adj_list[node]) {
+                if(!visited[successor] && is_cyclic_helper(adj_list, successor, visited, rec_stack))
+                    return true;
+                else if(rec_stack[successor])
+                    return true;
+            }
+        }
+
+        // Current node traversed. Remove it from recursion stack
+        rec_stack[node] = false;
+        return false;
+    }
+
+    // * Inspired from https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+    bool is_cyclic(unordered_map<char, vector<char>> adj_list, unordered_map<char, bool> &visited, unordered_map<char, bool> &rec_stack) {
+        for(auto it = visited.begin(); it != visited.end(); it++)
+            if(is_cyclic_helper(adj_list, it->first, visited, rec_stack))
+                return true;
+
+        return false;
     }
 };
 
